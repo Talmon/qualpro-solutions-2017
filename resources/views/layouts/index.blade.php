43aaa716dda918ecb6b8b5bb8041260b390d516e
Talmon <talimwakesi@gmail.com>
2017-08-17 15:36:04 +0300

    Carousel manenoz

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,72 +7,32 @@
        <div class="container-fluid">
        <div class="row">
        <div class="col-lg-8 col-sm-9 col-md-8 col-xs-8 ">
-       <h2>Our Projects</h2>
-       <div id="myCarousel" class="carousel slide" data-ride="carousel">
-           <!-- Indicators -->
-           <ol class="carousel-indicators">
-               <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-               <li data-target="#myCarousel" data-slide-to="1"></li>
-               <li data-target="#myCarousel" data-slide-to="2"></li>
-               <li data-target="#myCarousel" data-slide-to="3"></li>
-               <li data-target="#myCarousel" data-slide-to="4"></li>
-           </ol>
-
-           <!-- Wrapper for slides -->
-           <div class="carousel-inner">
-
-               <div class="item active">
-                   <img src="../images/projects-4.jpg" alt=""  style="width:100%;">
-                   <div class="carousel-caption">
-                      <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 1 </h4></button> </a>
-                       <p>Our Favourite</p>
-                   </div>
+           <div class="jR3DCarouselGallery">
+               <div class="mySlide">
+                   <img src="/images/projects-1.jpg" alt="">
                </div>
-
-               <div class="item">
-                   <img src="../images/projects-5.jpg" alt=""  style="width:100%;">
-                   <div class="carousel-caption">
-                       <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 2 </h4></button> </a>
-                       <p>Some Description</p>
-                   </div>
+               <div class="mySlide">
+                   <img src="/images/projects-2.jpg" alt="">
                </div>
-
-               <div class="item">
-                   <img src="../images/projects-6.jpg" alt=""  style="width:100%;">
-                   <div class="carousel-caption">
-                       <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 3 </h4></button> </a>
-                       <p>Another Description</p>
-                   </div>
+               <div class="mySlide">
+                   <img src="/images/projects-3.jpg" alt="">
                </div>
-
-               <div class="item">
-                   <img src="../images/projects-1.jpg" alt=""  style="width:100%;">
-                   <div class="carousel-caption">
-                       <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 4 </h4></button> </a>
-                       <p>Description here</p>
-                   </div>
+               <div class="mySlide">
+                   <img src="/images/projects-4.jpg" alt="">
                </div>
-
-               <div class="item">
-                   <img src="../images/projects-3.jpg" alt=""  style="width:100%;">
-                   <div class="carousel-caption">
-                       <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 5 </h4></button> </a>
-                       <p>Descrition ends</p>
-                   </div>
+               <div class="mySlide">
+                   <img src="/images/projects-5.jpg" alt="">
                </div>
+               <script>
 
+                   var myjR3DCarousel = $('.jR3DCarouselGallery').jR3DCarousel({
+                       slideClass: 'mySlide',
+                   });
+               </script>
            </div>
 
-           <!-- Left and right controls -->
-           <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-               <span class="glyphicon glyphicon-chevron-left"></span>
-               <span class="sr-only">Previous</span>
-           </a>
-           <a class="right carousel-control" href="#myCarousel" data-slide="next">
-               <span class="glyphicon glyphicon-chevron-right"></span>
-               <span class="sr-only">Next</span>
-           </a>
-       </div>
+
+
        </div>
 
            <div class="col-lg-4 col-sm-3 col-md-4 col-xs-4  blog-sidebar">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -107,3 +107,72 @@ col-sm-3 col-md-4
 
     </div>
 </div>
+
+
+
+
+<h2>Our Projects</h2>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+        <li data-target="#myCarousel" data-slide-to="4"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+        <div class="item active">
+            <img src="../images/projects-4.jpg" alt=""  style="width:100%;">
+            <div class="carousel-caption">
+                <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 1 </h4></button> </a>
+                <p>Our Favourite</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="../images/projects-5.jpg" alt=""  style="width:100%;">
+            <div class="carousel-caption">
+                <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 2 </h4></button> </a>
+                <p>Some Description</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="../images/projects-6.jpg" alt=""  style="width:100%;">
+            <div class="carousel-caption">
+                <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 3 </h4></button> </a>
+                <p>Another Description</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="../images/projects-1.jpg" alt=""  style="width:100%;">
+            <div class="carousel-caption">
+                <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 4 </h4></button> </a>
+                <p>Description here</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="../images/projects-3.jpg" alt=""  style="width:100%;">
+            <div class="carousel-caption">
+                <a href="#"> <button type="button" class="btn btn-primary"><h4> Project 5 </h4></button> </a>
+                <p>Descrition ends</p>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
